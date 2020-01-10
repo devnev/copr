e30c9a4c58700d207a53e3413dc444fbe4d8b135
Mark Nevill <mark.nevill@gmail.com>
2020-01-10 16:18:06 +0000

    Go module with github dep.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/devnev/copr
+
+go 1.13
+
+require github.com/google/go-github/v29 v29.0.2
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import _ "github.com/google/go-github/v29/github"
+
+func main() {
+	fmt.Println("Hello")
+}
